4ccdc35deec07b29bd63cfff9274496fcdc6915b
NicaZadirieva <no.demchenko@yandex.ru>
2026-02-12 10:38:51 +0300

    feat: калькулятор без настроенного логгера

---
+++ calculator/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using calculator.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace calculator
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // 1. Настраиваем контейнер
+            var services = new ServiceCollection();
+            
+
+            
+            // 4. Запрашиваем экземпляр CalculatorService<double>
+            using var loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder.AddConsole();
+                builder.AddDebug();
+            });
+
+            var logger = loggerFactory.CreateLogger<CalculatorService<double>>();
+            var calcService = new CalculatorService<double>(logger); // явная передача
+
+            // 5. Используем
+            var sum = calcService.Divide(1, 0);
+            Console.WriteLine(sum);
+
+        }
+    }
+}
---
+++ calculator/Services/CalculatorService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Numerics;
+using System.Text;
+using Microsoft.Extensions.Logging;
+
+namespace calculator.Services
+{
+    internal class CalculatorService<T> where T : INumber<T>
+    {
+        private readonly ILogger<CalculatorService<T>> _logger;
+
+        public CalculatorService(ILogger<CalculatorService<T>> logger)
+        {
+            _logger = logger;
+        }
+
+        public T Add(T a, T b)
+        {
+            return a + b;
+        }
+        public T? Divide(T a, T b)
+        {
+            try
+            {
+                return a / b;
+            }
+            catch (DivideByZeroException ex)
+            {
+                _logger.LogError("b == 0. Нельзя совершить операцию");
+                return default;
+            }
+        }
+    }
+}
